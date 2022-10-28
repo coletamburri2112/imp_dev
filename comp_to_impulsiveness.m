ee3b46809924115f5f37df7768809b0044d8d6c2
coletamburri2112 <cole.tamburri@colorado.edu>
2022-10-28 12:02:18 -0600

    formatting edits, imp comp

--- comp_to_impulsiveness.m
+++ comp_to_impulsiveness.m
@@ -90,16 +90,23 @@ recmax_low3 = recmaximp(pat_in3,:);
 % set(gcf,'Position',[300 300 550 550])
 % idx = isfinite(recmax(:,1)) & isfinite(curly_I);
 % fitted0 = fit(recmax(idx,1)/1e18,curly_I(idx),'m*x+b'); 
+% 
 % k=plot(fitted0,'red');
 % hold on
 % set(k,'LineWidth',2)
 % scatter(recmax(:,1)/1e18,curly_I,200,curly_I,'.');
 % grid on
-% xlabel('Reconnection Rate [$$10^{18} Mx/s$$]','fontsize',15,'interpreter','latex')
-% ylabel('Impulsiveness Index [$$ln(min^{-1})$$]','fontsize',15,'interpreter','latex')
+% xlabel('\textsf{Reconnection Rate} [$$10^{18} Mx/s$$]','fontsize',20,'interpreter','latex')
+% ylabel('\textsf{Impulsiveness Index} [$$ln(min^{-1})$$]','fontsize',20,'interpreter','latex')
+% ax=gca;
+% ax.FontSize = 20;
 % xlim([0 35])
 % legend off
-% title('$$i$$ vs. Reconnection Rate, $$r^2 = 0.029$$','fontsize',25,'interpreter','latex')
+% a=colorbar;
+% hl = ylabel(a,'\textsf{Impulsiveness}','fontsize',20,'interpreter','latex');
+% 
+% 
+% title('$$i$$ \textsf{vs. Reconnection Rate,} $$r^2 = 0.029$$','fontsize',25,'interpreter','latex')
 
 % 
 % % pbaspect([1 1 1])
@@ -302,34 +309,37 @@ end
 % saveas(n,'/Users/owner/Desktop/Research/MAT_SOURCE/impcomp/posnegrecrate_c_and_m.png');
 
 % % % % QPP PERIOD AND I
-n=figure(1);
-clf
-colormap cool
-set(gcf,'Position',[300 300 500 500])
-idx2 = (log(qpp_peron) > 4.5);
-qpp_peron(idx2)
-qpp_peron(idx2) = NaN;
-scatter(log(qpp_peron/60),curly_I,200,curly_I,'.');
-idx = isfinite(curly_I) & isfinite(qpp_peron);
-fitted2 = fit(log(qpp_peron(idx)/60),curly_I(idx),'m*x+b'); 
-
-
-
-
-
-title('$$i$$ vs. QPP Period ($$r^2 = 0.225$$)','FontSize',22,'interpreter','latex');
-hold on
-p=plot(fitted2,'red');
-set(p,'LineWidth',2)
-ylabel('Impulsiveness Index [$$ln(min^{-1})$$]','fontsize',15,'interpreter','latex');
-xlabel('QPP Period [ln(min)]','fontsize',15,'interpreter','latex');
-dim = [.6 .55 .3 .3];
-str = {['m = ',num2str(fitted2.m)],['b = ',num2str(fitted2.b)],['r^2 = 0.225']};
-%annotation('textbox',dim,'String',str,'FitBoxToText','on','fontsize',14);
-grid on
-legend off
+% n=figure(1);
+% clf
+% colormap cool
+% set(gcf,'Position',[300 300 500 500])
+% idx2 = (log(qpp_peron) > 4.5);
+% qpp_peron(idx2)
+% qpp_peron(idx2) = NaN;
+% scatter(log(qpp_peron/60),curly_I,200,curly_I,'.');
+% idx = isfinite(curly_I) & isfinite(qpp_peron);
+% fitted2 = fit(log(qpp_peron(idx)/60),curly_I(idx),'m*x+b'); 
+% 
+% 
+% 
+% 
+% 
+% title('$$i$$ \textsf{vs. QPP Period} ($$r^2 = 0.225$$)','FontSize',28,'interpreter','latex');
+% hold on
+% p=plot(fitted2,'red');
+% set(p,'LineWidth',2)
+% ylabel('\textsf{Impulsiveness Index} [$$ln(min^{-1})$$]','fontsize',20,'interpreter','latex');
+% xlabel('\textsf{QPP Period} [$$ln(min)$$]','fontsize',20,'interpreter','latex');
+% dim = [.6 .55 .3 .3];
+% str = {['m = ',num2str(fitted2.m)],['b = ',num2str(fitted2.b)],['r^2 = 0.225']};
+% ax=gca;
+% ax.FontSize=20;
+% %annotation('textbox',dim,'String',str,'FitBoxToText','on','fontsize',14);
+% grid on
+% legend off
 % pbaspect([1 1 1])
-
+% a=colorbar;
+% hl = ylabel(a,'\textsf{Impulsiveness}','fontsize',20,'interpreter','latex');
 
 % saveas(n,'/Users/owner/Desktop/Research/MAT_SOURCE/impcomp/imp_qppper_more.fig');
 % saveas(n,'/Users/owner/Desktop/Research/MAT_SOURCE/impcomp/imp_qppper_more.png');
@@ -483,25 +493,30 @@ legend off
 
 
 % %%comparison of rise to decay phase
-% idx = isfinite(curly_I) & isfinite(rise_dur);
-% l=figure(5)
-% set(gcf,'Position',[300 300 500 500])
-% clf
-% colormap cool
-% 
-% scatter(dec_dur_nonan/60,rise_dur_nonan/60,200,curly_I(idx),'.');
-% ylabel('Rise Phase Duration [min]','fontsize',15,'interpreter','latex')
-% xlabel('Decay Phase Duration [min]','fontsize',15,'interpreter','latex')
-% title('Rise vs. Decay Phase Duration, $r^2 = 0.076$','fontsize',20,'interpreter','latex')
-% 
-% 
-% p = polyfit(dec_dur_nonan/60,rise_dur_nonan/60,1);
-% yfit = polyval(p,dec_dur_nonan/60);
-% hold on
-% grid on
-% k = plot(dec_dur_nonan/60,yfit,'red');
-% set(k,'LineWidth',2)
-% pbaspect([1 1 1])
+idx = isfinite(curly_I) & isfinite(rise_dur);
+l=figure(5)
+set(gcf,'Position',[300 300 600 600])
+clf
+colormap cool
+
+scatter(dec_dur_nonan/60,rise_dur_nonan/60,200,curly_I(idx),'.');
+ax = gca;
+ax.FontSize=15;
+ylabel('\textsf{Rise Phase Duration} $[min]$','fontsize',20,'interpreter','latex')
+xlabel('\textsf{Decay Phase Duration} $[min]$','fontsize',20,'interpreter','latex')
+title('$t_{rise}$ \textsf{vs.} $t_{decay}$ ($r^2 = 0.076$)','fontsize',30,'interpreter','latex')
+
+
+p = polyfit(dec_dur_nonan/60,rise_dur_nonan/60,1);
+yfit = polyval(p,dec_dur_nonan/60);
+hold on
+grid on
+k = plot(dec_dur_nonan/60,yfit,'red');
+set(k,'LineWidth',2)
+pbaspect([1 1 1])
+a=colorbar;
+hl = ylabel(a,'\textsf{Impulsiveness}','fontsize',20,'interpreter','latex');
+
 
 % saveas(l,'/Users/owner/Desktop/Research/MAT_SOURCE/impcomp/rise_dec_comp_more.fig');
 % saveas(l,'/Users/owner/Desktop/Research/MAT_SOURCE/impcomp/rise_dec_comp_more.png');
@@ -525,9 +540,9 @@ legend off
 % %k=plot(fitted0,'red');
 % %set(k,'LineWidth',2)
 % legend off
-% ylabel('Impulsiveness Index [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
-% xlabel('Rise Phase Duration [$$ln(min)$$]','fontsize',14,'interpreter','latex');
-% title(['$$i$$ vs. $$t_{rise}$$ ($$r^2 = 0.053$$)'],'FontSize',22,'interpreter','latex');
+% ylabel('\textsf{Impulsiveness Index} [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
+% xlabel('\textsf{Rise Phase Duration} [$$ln(min)$$]','fontsize',14,'interpreter','latex');
+% 
 % 
 % % dim = [.7 .55 .6 .6];
 % % % str = {['m = ',num2str(fitted0.m)],['b = ',num2str(fitted0.b)]};
@@ -540,6 +555,10 @@ legend off
 % % h=figure(3);
 % % clf
 % colormap cool
+% ax=gca;
+% ax.FontSize=15;
+% title(['$$i$$ \textsf{vs.} $$t_{rise}$$ ($$r^2 = 0.053$$)'],'FontSize',22,'interpreter','latex');
+% 
 % subplot(1,3,2)
 % set(gcf,'Position',[300 300 1200 500])
 % scatter(log(dec_dur/60),curly_I,200,curly_I,'.');
@@ -557,17 +576,20 @@ legend off
 % str = {['m = ',num2str(fitted1.m)],['b = ',num2str(fitted1.b)]};
 % %annotation('textbox',dim,'String',str,'FitBoxToText','on');
 % legend off
-% ylabel('Impulsiveness Index [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
-% xlabel('Decay Phase Duration [$$ln(min)$$]','fontsize',14,'interpreter','latex');
-% title('$$i$$ vs. $$t_{decay}$$ ($$r^2 = 0.083$$)','FontSize',22,'interpreter','latex');
-% % itle('$$i$$ vs. QPP Period ($$r^2 = 0.288$$)','FontSize',22,'interpreter','latex');
+% ylabel('\textsf{Impulsiveness Index} [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
+% xlabel('\textsf{Decay Phase Duration} [$$ln(min)$$]','fontsize',14,'interpreter','latex');
+
+% itle('$$i$$ vs. QPP Period ($$r^2 = 0.288$$)','FontSize',22,'interpreter','latex');
+% 
+% saveas(h,'/Users/owner/Desktop/imp_decaydur_more_loglog.fig');
+% saveas(h,'/Users/owner/Desktop/imp_decaydur_more_loglog.png');
+% saveas(h,'/Users/owner/Desktop/fig7.png');
 % % 
-% % saveas(h,'/Users/owner/Desktop/imp_decaydur_more_loglog.fig');
-% % saveas(h,'/Users/owner/Desktop/imp_decaydur_more_loglog.png');
-% % saveas(h,'/Users/owner/Desktop/fig7.png');
-% % % 
-% % l=figure(4);
-% % clf
+% l=figure(4);
+% clf
+% ax=gca;
+% ax.FontSize=15;
+% title('$$i$$ \textsf{vs.} $$t_{decay}$$ ($$r^2 = 0.083$$)','FontSize',22,'interpreter','latex');
 % subplot(1,3,3)
 % set(gcf,'Position',[300 300 1200 500])
 % scatter(log(overall_dur/60),curly_I,200,curly_I,'.');
@@ -585,9 +607,15 @@ legend off
 % str = {['m = ',num2str(fitted2.m)],['b = ',num2str(fitted2.b)],['r^2 = 0.1052']};
 % %annotation('textbox',dim,'String',str,'FitBoxToText','on','fontsize',12);
 % legend off
-% ylabel('Impulsiveness Index [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
-% xlabel('Flare Duration [$$ln(min)$$]','fontsize',14,'interpreter','latex');
-% title(['$$i$$ vs. $$t_{flare}$$ ($$r^2 = 0.132$$)'],'FontSize',22,'interpreter','latex');
+% ylabel('\textsf{Impulsiveness Index} [$$ln(min^{-1})$$]','fontsize',14,'interpreter','latex');
+% xlabel('\textsf{Flare Duration} [$$ln(min)$$]','fontsize',14,'interpreter','latex');
+% 
+% ax=gca;
+% ax.FontSize=15;
+% hp4 = get(subplot(1,3,3),'Position')
+% a=colorbar('Position', [hp4(1)+.23  hp4(2)+.155  0.01 hp4(4)*0.625])
+% hl = ylabel(a,'\textsf{Impulsiveness}','fontsize',20,'interpreter','latex');
+% title(['$$i$$ \textsf{vs.} $$t_{flare}$$ ($$r^2 = 0.132$$)'],'FontSize',22,'interpreter','latex');
 % % % 
 % % 
 % % saveas(l,'/Users/owner/Desktop/imp_overdur_more_loglog.fig');
